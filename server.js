a6e7d2bba9aac29d4d4e0df89567643865d8aaea
Alisher <kodirjonovalisher@gmail.com>
2026-08-27 23:05:46 +0900

    feat: master author router ejs page

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ console.log("Web server is starting...");
 const express = require("express");
 const app = express();
 const http = require("http");
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf-8", (err, data) => {
+    if (err) {
+        console.log("Error:", err);
+    } else {
+        user = JSON.parse(data);
+    }
+});
 
 // 1 Kirish code
 
@@ -17,8 +27,13 @@ app.set("view engine", "ejs");
 
 // 4 Routing code
 app.post("/create-item", (req, res) => {
-    console.log(req);
-    res.json({ message: "Test success" });
+
+    //TO DO: code with database here
+});
+
+    app.get("/author", (req, res) => {
+        res.render("author", { user: user});
+
 });
 
 app.get("/", function (req, res) { 
